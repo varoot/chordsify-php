52bc262d26d8a2350852f98675b43029b0d37eae
Varoot Phasuthadol <varoot@gmail.com>
2014-06-14 01:19:59 -0400

    Test more songs

--- songsheet.php
+++ songsheet.php
@@ -18,11 +18,35 @@ function load_song($title, $key)
 }
 
 $sheet = new Chordsify\SongSheet();
+//$sheet->debug = TRUE;
 
+/*
 $sheet->add(load_song('10000 Reasons', 'G'));
 $sheet->add(load_song('How Great Is Our God', 'A'));
 $sheet->add(load_song('I Could Sing of Your Love Forever', 'E'));
 $sheet->add(load_song('Your Presence is Heaven', 'A'));
 $sheet->add(load_song('Once Again', 'G'));
+*/
 
-$sheet->pdf_output('F');
+/*
+$sheet->add(load_song('All Who Are Thirsty', 'E'));
+$sheet->add(load_song('Come to the Water', 'G'));
+$sheet->add(load_song('Give Thanks', 'G'));
+$sheet->add(load_song('The Wonderful Cross', 'D'));
+$sheet->add(load_song('The Stand', 'E'));
+*/
+
+$sheet->add(load_song('Jesus at the Center', 'E'));
+$sheet->add(load_song('All My Fountain', 'D'));
+$sheet->add(load_song('Heart of Worship', 'D'));
+$sheet->add(load_song('Nothing but the Blood', 'E'));
+$sheet->add(load_song('Your Presence is Heaven', 'A'));
+
+if ($sheet->debug)
+{
+	$sheet->pdf_output('F');
+}
+else
+{
+	$sheet->pdf_output();
+}
