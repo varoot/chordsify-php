0f793f876cf7a2b6de981300a5915426e0a1c919
Varoot Phasuthadol <varoot@gmail.com>
2014-08-20 02:33:43 +0700

    Move config to style

--- Chordsify/Config.php
+++ Chordsify/Config.php
@@ -1,8 +1,6 @@
 <?php
 namespace Chordsify;
 
-use Symfony\Component\Yaml\Yaml;
-
 class Config
 {
     public static $sections = [
@@ -12,19 +10,7 @@ class Config
 
     // PDF for SongSheet
     public static $font_dir = '../fonts/';
-    public static $pdf_columns = 2;
-
-    /* Column width is fixed to help with consistency among A4 and Letter paper size */
-    public static $pdf_column_width = 230;
-
-    /* For auto: 2 copies for 1 and 2 columns, 1 for 3+ */
-    public static $pdf_copies = 'auto';
-
-    /* Note: margin is only used for top & bottom
-       Left & right is calculated from column width */
-    public static $pdf_margin = 36;
-    public static $pdf_size = 'Letter'; // Default size for PDF
-    public static $pdf_style = 'center';
+    public static $pdfStyleSheet = 'styles.yaml';
 
     public static function loadStyle($style_name)
     {
---
+++ Chordsify/PDFStyle.php
@@ -0,0 +1,76 @@
+<?php
+namespace Chordsify;
+
+use Symfony\Component\Yaml\Yaml;
+
+trait PDFStyle
+{
+    // Fonts loaded
+    protected $fonts = [];
+
+    // Stylesheet
+    protected $styleSheet = [];
+
+    // Style of current element
+    protected $style = [];
+
+    abstract public function pdf();
+
+    public function loadStyleSheet($styleName)
+    {
+        $allStyles = Yaml::parse(realpath(__DIR__.'/'.Config::$pdfStyleSheet));
+
+        if ( ! array_key_exists($styleName, $allStyles))
+            return $allStyles['default'];
+
+        $this->styleSheet = array_merge($allStyles['default'], $allStyles[$styleName]);
+
+        return $this;
+    }
+
+    protected function addFont($font)
+    {
+        $fontFile = realpath(__DIR__.'/'.Config::$font_dir.$font);
+        if (is_file($fontFile)) {
+            return $this->fonts[$font] = $this->pdf()->addTTFfont($fontFile);
+        }
+        return $font;
+    }
+
+    public function setFont($font, $size)
+    {
+        // Check if font is loaded
+        if (array_key_exists($font, $this->fonts)) {
+            $font = $this->fonts[$font];
+        } else {
+            // Load font if exists
+            $font = $this->addFont($font);
+        }
+
+        $this->pdf()->SetFont($font, '', $size);
+
+        return $this;
+    }
+
+    public function setStyle($path)
+    {
+        $path = explode('.', $path);
+        $style = $tree = $this->styleSheet;
+
+        foreach ($path as $level) {
+            if ( ! isset($tree[$level]))
+                break;
+
+            $tree = $tree[$level];
+            $style = array_merge($style, $tree);
+        }
+
+        $this->style = array_filter($style, function($x) { return ! is_array($x); });
+
+        if ($this->pdf()) {
+            $this->setFont($this->style['font'], $this->style['fontSize']);
+        }
+
+        return $this;
+    }
+}
